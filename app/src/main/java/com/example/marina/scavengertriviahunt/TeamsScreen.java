9e2ddb5178a3447ef94099f1d8d544a593e3acc9
mschong <marinachong96@gmail.com>
2018-04-03 18:49:42 -0600

    Activities fixed

--- app/src/main/java/com/example/marina/scavengertriviahunt/TeamsScreen.java
+++ app/src/main/java/com/example/marina/scavengertriviahunt/TeamsScreen.java
@@ -1,7 +1,9 @@
 package com.example.marina.scavengertriviahunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TeamsScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class TeamsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams_screen);
+    }
+
+    public void okClicked(View view){
+        Intent i = new Intent(TeamsScreen.this, StudentClue.class);
+        startActivity(i);
     }
 }
